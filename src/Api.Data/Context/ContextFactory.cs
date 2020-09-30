c00cdea65f7adee33c1194fdab48f77639f7c5f7
Matheus de Campos <matheus.campos@edu.unifil.br>
2020-09-29 23:55:24 -0300

    feat: update

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrations
+            var connectionString = "Server=localhost;Port=3306;Database=DbAPI;Uid=root;Pwd=admin";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+             optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
